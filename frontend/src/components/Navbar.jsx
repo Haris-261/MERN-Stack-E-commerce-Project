b7301865d97ebbbe2d2d9d39bb7210f48e69ca60
Haris-261 <Haris200408@gmail.com>
2026-06-07 02:11:19 +0500

    First Commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { ShopContext } from "../context/ShopContext.jsx";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch, getCartCount, token, setToken, navigate, setCartItems } = useContext(ShopContext);
+  const { setShowSearch, getCartCount, token, setToken, navigate, setCartItems, } = useContext(ShopContext);
 
   const logout = () => {
     // Implement logout functionality
@@ -46,10 +46,10 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6">
-        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className="w-5 cursor_pointer" alt="" />
+        <img onClick={() => {setShowSearch(true); navigate('/collection') } } src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
 
         <div className="group relative">
-          <img onClick={() => token ? null : navigate("/login")} src={assets.profile_icon} className="w-5 cursor_pointer" alt="" />
+          <img onClick={() => token ? null : navigate("/login")} src={assets.profile_icon} className="w-5 cursor-pointer" alt="" />
           <div className="group-hover:block hidden absolute dropdewn-menu right-0 pt-4">
             {token && <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
